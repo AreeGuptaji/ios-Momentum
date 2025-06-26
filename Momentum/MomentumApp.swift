4393f9964fbc3f96c862229be323b8baafaed0a9
Aman Gupta <00amangpt00@gmail.com>
2025-06-26 16:50:44 +0530

    Initial Commit

---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Aman Gupta on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
